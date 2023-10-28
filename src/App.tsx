b743b890cd21f39a12681061a5bf21213f48ff1b
Logan Gibler <logan@DESKTOP-8TT4RDK.attlocal.net>
2023-10-27 20:42:00 -0400

    first commit, working on boiler

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./globals.css"
+import { Routes, Route } from "react-router-dom"
+
+
+
+const App = () => {
+  return (
+   <main className="flex h-screen">
+
+   </main>
+  )
+}
+
+export default App
